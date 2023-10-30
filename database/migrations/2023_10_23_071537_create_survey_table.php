4712e83f631cbfe2f9bf2801747b3835523c82e9
Kevin <darmawankevinsanadi@gmail.com>
2023-10-30 20:08:10 +0700

    don't look

--- database/migrations/2023_10_23_071537_create_survey_table.php
+++ database/migrations/2023_10_23_071537_create_survey_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('survey', function (Blueprint $table) {
+        Schema::create('surveys', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
